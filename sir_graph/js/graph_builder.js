d32bf41389e5e438e66137330800bb7a1411c44c
Georgi Kiryakov <george.kiryakov@gmail.com>
2016-04-25 14:42:51 +0300

    Implementation of Barabasi–Albert model, RandomGraph generator and colorMutate function

--- sir_graph/js/graph_builder.js
+++ sir_graph/js/graph_builder.js
@@ -1,41 +1,123 @@
+function colorMutate(c, probability) {
+  var color = c;
+  if (Math.random() < probability)
+    color = color.replace(color[Math.floor(Math.random()*6 + 1)], Math.floor(Math.random()*16).toString(16));
+
+  return color;
+}
+
+function colorCrossOver(cA, cB) {
+  var color = "";
+  if (cA == cB)
+    color = "#" + cA.substr(4, 3) + cA.substr(1, 3);
+  else if (Math.random() >=0.5)
+    color = "#" + cA.substr(1, 3) + cB.substr(4, 3);
+  else
+    color = "#" + cB.substr(1, 3) + cA.substr(4, 3);
+
+  return color;
+}
+
 var GraphBuilder = {
+  RandomGraph: function(graph, nodes, edges) {
+    var that = self;
+    that.prefix = 'rand_';
 
-  BarabasiAlbert: function(graph, init_num_nodes, final_num_nodes) {
-    if (init_num_nodes > final_num_nodes)
-      init_num_nodes = final_num_nodes;
-
-    for (var i = 0; i < init_num_nodes; i++) {
-      graph.addNodes('ba_' + i);
+    for (var i = 0; i < nodes; i++) {
+      graph.addNodes(that.prefix + i);
     }
 
-    for (var i = 0; i < init_num_nodes; i++) {
-      for (var j = 0; j < init_num_nodes; j++)
-        graph.addEdges(['ba_' + i, 'ba_' + j, {color: '#000'}]);
+    for (var i=0; i < edges; i++) {
+      var randS = Math.floor(Math.random()*nodes);
+      var randT = Math.floor(Math.random()*nodes);
+      while (randS == randT || (graph.adjacency[that.prefix + randS] != undefined && graph.adjacency[that.prefix + randS][that.prefix + randT] != undefined))
+        randT = Math.floor(Math.random()*nodes);
+
+      graph.addEdges(
+        [that.prefix + randS, that.prefix + randT],
+        [that.prefix + randT, that.prefix + randS]
+      );
     }
-    return;
-
-    for (var i = init_num_nodes; i < final_num_nodes; i++) {
-      var node = new Springy.Node("ba_" + i, {label: "ba_" + i});
-      graph.addNode(node);
-
-      var target = Math.random();
-
-      // connect to other
-      for (var j = 0; j < graph.nodes.length; j++) {
-        var prob = 2*graph.getNodeDegree(graph.nodes[j]) / graph.edges.length;
-
-        if (target < prob) {
-          graph.addEdges(
-            ["ba_" + i, "ba_" + j, {color: '#000'}],
-            ["ba_" + j, "ba_" + i, {color: '#000'}]
-          );
-        }
-
-      }
-
-    }
-
-    return graph;
   }
 
+};
+
+/**
+* @param graph Reference to graph
+* @param m0 The number of nodes in the seed network.
+* @param m The number of edges to add at each time step.
+* @param final_size The number of nodes in generated networks
+*/
+GraphBuilder.BarabasiAlbert = function (graph, m0, m, final_size) {
+  var that = self;
+  that.prefix = "ba__";
+
+  that.graph = graph;
+  that.m0 = m0;
+  that.m = m;
+  that.final_size = final_size;
+
+  /** @param n Source Node */
+  that.addPreferentialEdge = function (n, n_id) {
+    var G = that.graph;
+    var target = Math.random();
+    var probability = 0;
+    var edges_sum = G.edges.length;
+
+    for (var i = 0; i < G.nodes.length; i++) {
+
+      probability += G.getNodeDegree(G.nodes[i]) / edges_sum;
+
+      if (target <= probability) {
+        if (G.nodes[n_id].data.color == undefined)
+          G.nodes[n_id].data.color = colorMutate(G.nodes[i].data.color, 0.5);
+
+        G.addEdges(
+          [that.prefix + n_id, that.prefix + i],
+          [that.prefix + i, that.prefix + n_id]
+        );
+        return;
+      }
+    }
+  }
+
+  // initialize graph
+  if (that.m0 > that.fina_size)
+    that.m0 = that.fina_size;
+
+  for (var i = 0; i < that.m0; i++) {
+    var node = new Springy.Node(that.prefix + i, {
+      label: that.prefix + i,
+      color: '#' + Math.floor(Math.random()*16777215).toString(16),
+      }
+    );
+    that.graph.addNode(node);
+  }
+
+  for (var i = 0; i < that.m0; i++) {
+    for (var j = 0; j < that.m0; j++)
+      that.graph.addEdges([that.prefix + i, that.prefix + j]);
+  }
+
+  var nodeInterval = setInterval(function() {
+    node_size = that.graph.nodes.length;
+
+    if (node_size > that.final_size) {
+      clearInterval(nodeInterval);
+      return;
+    }
+
+    var node = new Springy.Node(
+                that.prefix + node_size, {label: that.prefix + node_size}
+              );
+
+    that.graph.addNode(node);
+
+    // add m number of edges
+    for (var m = 0; m < that.m; m++)
+        that.addPreferentialEdge(node, node_size); // node_size is current id
+  }, 5);
+
+
+  return that;
 };
