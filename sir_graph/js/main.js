6e49211b017c4ae4ce2e98b75aa0c98e926b213a
Georgi Kiryakov <george.kiryakov@gmail.com>
2016-04-25 14:46:16 +0300

    Demo usage of GraphBuilder

--- sir_graph/js/main.js
+++ sir_graph/js/main.js
@@ -1,48 +1,16 @@
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-function chageColor(g) {
-  r_index = Math.floor(Math.random()*graph.edges.length)
-  graph.edges[r_index].data.color = getRandomColor();
-}
-
 var graph = new Springy.Graph();
-// graph.addNodes('Dennis', 'Michael', 'Jessica', 'Timothy', 'Barbara')
-// graph.addNodes('Amphitryon', 'Alcmene', 'Iphicles', 'Heracles');
-// graph.addEdges(
-//   ['Dennis', 'Michael', {color: '#00A0B0', label: 'Foo bar'}],
-//   ['Michael', 'Dennis', {color: '#6A4A3C'}],
-//   ['Michael', 'Jessica', {color: '#CC333F'}],
-//   ['Jessica', 'Barbara', {color: '#EB6841'}],
-//   ['Michael', 'Timothy', {color: '#EDC951'}],
-//   ['Amphitryon', 'Alcmene', {color: '#7DBE3C'}],
-//   ['Alcmene', 'Amphitryon', {color: '#BE7D3C'}],
-//   ['Amphitryon', 'Iphicles'],
-//   ['Amphitryon', 'Heracles'],
-//   ['Barbara', 'Timothy', {color: '#6A4A3C'}]
-// );
-
-GraphBuilder.BarabasiAlbert(graph, 2, 50);
-
+GraphBuilder.BarabasiAlbert(graph, 1, 1, 250);
+//GraphBuilder.RandomGraph(graph, 250, 230);
 
 jQuery(function(){
   var springy = jQuery('#sir_graph').springy({
     graph: graph,
-    stiffness: 10,
-    repulsion: 10,
-    //minEnergyThreshold: 0.1
+    stiffness: 100,
+    repulsion: 100,
+    minEnergyThreshold: 0.01,
+    //damping: 0.6
   });
-  console.log(springy)
 
-  setInterval(function() {
-    chageColor(graph);
-    graph.notify();
-  }, 10);
-
+  window.g_springy = springy;
+  console.log(springy);
 });
