a73b320892d1344c45be92b8dd36caecd84a0294
Nikola Stoyanov <ns.barzakov@gmail.com>
2016-06-02 09:47:28 +0300

    Changed fit_sir to modify the `step` parameter when appropriate.

--- sir/fit_sir.py
+++ sir/fit_sir.py
@@ -3,6 +3,9 @@ import numpy as np
 import math
 import csv
 
+EPSILON = 0.000001
+INV_EPSILON = 1 / EPSILON
+
 class FitSir:
     def __init__(self, sir, data, time, step, precision):
         self.sir = sir
@@ -20,20 +23,25 @@ class FitSir:
     def cost(self, beta):
         self.sir.infect_prob = beta
         sir_results = self.sir.simulate_points(self.time, sim_step=self.step)
-        return self.error(sir_results.I)
+        return self.error(sir_results.S)
 
     def cost_derivative(self, beta):
-        return (self.cost(beta + 0.000001) - self.cost(beta)) / 0.000001
+        return (self.cost(beta + EPSILON) - self.cost(beta)) * INV_EPSILON
 
     def fit(self, beta, step):
         curr_step = 0
+        prev_cost = float("Inf")
 
         while True:
             curr_cost = self.cost_derivative(beta)
+
+            if abs(curr_cost) > abs(prev_cost):
+                step = step * 0.5
+
             beta = beta - step * curr_cost
 
             curr_step = curr_step + 1
-            if curr_step == 1000:
+            if curr_step == 100:
                 print("Simulating... beta={}".format(beta));
                 curr_step = 0
 
