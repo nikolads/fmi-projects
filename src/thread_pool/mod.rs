ff0d6bdcdd3ba68770b446e845ab2692b61530a0
Nikola Stoyanov <ns.barzakov@gmail.com>
2017-06-30 10:52:33 +0300

    Redesigned thread pool

--- src/thread_pool/mod.rs
+++ src/thread_pool/mod.rs
@@ -1,85 +1,101 @@
+extern crate crossbeam;
+
+use self::crossbeam::sync::chase_lev::{self, Steal, Stealer, Worker};
+
 use std::boxed::FnBox;
 use std::io;
-use std::sync::mpsc::{self, Sender};
-use std::thread::{self, Builder, JoinHandle};
+use std::sync::{Arc, Condvar, Mutex};
+use std::thread::Builder;
 
 enum Msg {
-    Exec(Box<FnBox() + Send + 'static>),
-    Exit
+    Run(Box<FnBox() + Send + 'static>),
+    Close
 }
 
 /// Thread pool
 pub struct Pool {
-    threads: Vec<(JoinHandle<()>, Sender<Msg>)>
+    threads: usize,
+    condvar: Arc<CondvarPair>,
+    queue: Worker<Msg>,
 }
 
 impl Pool {
     pub fn new(nthreads: usize) -> Result<Pool, io::Error> {
         let mut threads = Vec::new();
+        let (worker, stealer) = chase_lev::deque();
+        let condvar = Arc::new(CondvarPair::new());
 
         for i in 0..nthreads {
-            let (sender, receiver) = mpsc::channel();
-
-            let thread_main = move || {
-                loop {
-                    for msg in receiver.try_iter() {
-                        match msg {
-                            Msg::Exec(func) => func(),
-                            Msg::Exit => return,
-                        }
-                    }
-
-                    thread::park();
-                }
-            };
+            let stealer = stealer.clone();
+            let condvar = condvar.clone();
 
             let handle = Builder::new()
                 .name(format!("thread #{}", i))
-                .spawn(thread_main)?;
+                .spawn(move || { thread_main(stealer, condvar) })?;
 
-
-            threads.push((handle, sender));
+            threads.push(handle);
         };
 
-        Ok(Pool { threads })
+        Ok(Pool {
+            threads: nthreads,
+            condvar: condvar,
+            queue: worker,
+        })
     }
 
-    /// Return an iterator over the threads in this pool.
-    pub fn threads<'a>(&'a self) -> impl Iterator<Item = Handle<'a>> {
-        self.threads.iter().map(|handle| Handle { inner: handle })
-    }
-
-    /// Block and wait all threads in the pool to finish their scheduled tasks.
-    pub fn join(self) -> thread::Result<()> {
-        for &(ref handle, ref sender) in &self.threads {
-            let _ = sender.send(Msg::Exit);
-            handle.thread().unpark();
-        }
-
-        for (handle, _) in self.threads {
-            handle.join()?;
-        }
-
-        Ok(())
-    }
-}
-
-/// Handle to a thread in a thread pool.
-///
-/// Can be used to execute a function on this thread.
-pub struct Handle<'a> {
-    inner: &'a (JoinHandle<()>, Sender<Msg>),
-}
-
-impl<'a> Handle<'a> {
-    /// Schedule a function to be executed on the thread.
-    pub fn execute<F> (&self, func: F) where
+    pub fn spawn<F>(&mut self, task: F) where
         F: FnOnce() + Send + 'static
     {
-        let &(ref handle, ref sender) = self.inner;
+        self.queue.push(Msg::Run(Box::new(task)));
+        self.condvar.var.notify_one();
+    }
 
-        // if `send` fails then the thread must have paniced
-        sender.send(Msg::Exec(Box::new(func))).unwrap();
-        handle.thread().unpark();
+    /// Block until all tasks are done
+    pub fn wait(&self) {
+        unimplemented!();
+    }
+}
+
+impl Drop for Pool {
+    fn drop(&mut self) {
+        for _ in 0..self.threads {
+            self.queue.push(Msg::Close);
+        }
+        self.condvar.var.notify_all();
+    }
+}
+
+struct CondvarPair {
+    mutex: Mutex<()>,
+    var: Condvar,
+}
+
+impl CondvarPair {
+    fn new() -> Self {
+        CondvarPair {
+            mutex: Mutex::new(()),
+            var: Condvar::new(),
+        }
+    }
+}
+
+fn thread_main(stealer: Stealer<Msg>, condvar: Arc<CondvarPair>) {
+    loop {
+        match stealer.steal() {
+            Steal::Empty => {
+                let lock = condvar.mutex.lock().unwrap();
+                drop(condvar.var.wait(lock).unwrap());
+            },
+            Steal::Abort => {
+                // exponential_backoff()
+            },
+            Steal::Data(Msg::Run(func)) => {
+                func();
+            },
+            Steal::Data(Msg::Close) => {
+                // wake pool waiters
+                break;
+            }
+        }
     }
 }
